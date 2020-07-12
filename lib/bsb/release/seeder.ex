d2a6de9429ef1e5f4ea7f88babba8e38c464856b
Will Strinz <wstrinz@gmail.com>
2020-07-12 19:13:54 +0000

    docker compose and prod seeding

---
+++ lib/bsb/release/seeder.ex
@@ -0,0 +1,57 @@
+defmodule Bsb.Release.Seeder do
+  @moduledoc """
+  Release tasks for seeds.
+  """
+
+  require Logger
+
+  @app :bsb
+
+  @doc """
+  Seed seeds file for repo.
+  """
+  @spec seed(Ecto.Repo.t(), String.t()) :: :ok | {:error, any()}
+  def seed(repo, filename) do
+    load_app()
+
+    case Ecto.Migrator.with_repo(repo, &eval_seed(&1, filename)) do
+      {:ok, {:ok, _fun_return}, _apps} ->
+        :ok
+
+      {:ok, {:error, reason}, _apps} ->
+        Logger.error(reason)
+        {:error, reason}
+
+      {:error, term} ->
+        IO.warn(term, [])
+        {:error, term}
+    end
+  end
+
+  @spec eval_seed(Ecto.Repo.t(), String.t()) :: any()
+  defp eval_seed(repo, filename) do
+    seeds_file = get_path(repo, filename)
+
+    if File.regular?(seeds_file) do
+      {:ok, Code.eval_file(seeds_file)}
+    else
+      {:error, "Seeds file not found."}
+    end
+  end
+
+  @spec get_path(Ecto.Repo.t(), String.t()) :: String.t()
+  defp get_path(repo, filename) do
+    priv_dir = "#{:code.priv_dir(@app)}"
+
+    repo_underscore =
+      repo
+      |> Module.split()
+      |> List.last()
+      |> Macro.underscore()
+
+    Path.join([priv_dir, repo_underscore, filename])
+  end
+
+  @spec load_app() :: :ok | {:error, term()}
+  defp load_app(), do: Application.load(@app)
+end
